8c97f001ea9fdee21cb48ab80b8600048697a3e9
Vedant  Nandoskar <vedant.nandoskar@gmail.com>
2018-03-12 21:31:32 +0530

    Update OMNI_IMF.py

--- OMNI_IMF.py
+++ OMNI_IMF.py
@@ -5,14 +5,13 @@ import pandas as pd
 
 def listing():
     ##Takes the input and advances to print the respective data.
-    print("Enter Day(ranging from 1-365) and Hour(ranging from 0-23)")
+    print("Enter Day(ranging from 1-365) and Hour(ranging from 0-23) with a space in between.")
     k,l = input().split()
     k = int(k)
     l = int(l)
     n = (k-1)*24 + l
     return n
 
-##df.OMNI_DATA = df.OMNI_DATA.astype(float).fillna(0.0)
 def set_input():
     pd.set_option('display.height', 1000)
     pd.set_option('display.max_rows', 8762)
@@ -46,15 +45,16 @@ def main():
     set_input()
     print("\n", L)
     
-print("Enter 'full_data for entire data frame and 'specific_data' for specific frame'.")
-inp = input()
-if inp == "full_data":
-    allData()
-elif inp == "specific_data":
-    main()
-else:
-    print("Invalid Input.")
+def run():
+    print("Enter 'full_data for entire data frame and 'specific_data' for specific frame'.")
+    inp = input()
+    if inp == "full_data":
+        allData()
+    elif inp == "specific_data":
+        main()
+    else:
+        print("Invalid Input.")
 
-
+run()
    
 
